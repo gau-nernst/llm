2cd0f28920a2677f86f30446b976488f6ece49bb
Thien Tran <gau.nernst@yahoo.com.sg>
2025-10-18 14:56:42 +0800

    merge qkv and gate_up

--- modelling/qwen3.py
+++ modelling/qwen3.py
@@ -12,25 +12,37 @@ from .rope import apply_rope, compute_rope
 from .utils import load_hf_state_dict
 
 
+def make_merge_hook(old_keys: list[str], new_key: str):
+    def hook(module, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs):
+        if f"{prefix}{old_keys[0]}_proj.weight" not in state_dict:
+            return
+        w_list = [state_dict.pop(f"{prefix}{key}_proj.weight") for key in old_keys]
+        state_dict[f"{prefix}{new_key}_proj.weight"] = torch.cat(w_list, dim=0)
+
+    return hook
+
+
 class Qwen3Attention(nn.Module):
     def __init__(self, cfg: Qwen3Config) -> None:
         super().__init__()
         self.head_dim = cfg.head_dim
         self.attention_dropout = cfg.attention_dropout
+        self.num_qo_heads = cfg.num_attention_heads
+        self.num_kv_heads = cfg.num_key_value_heads
         qo_dim = cfg.num_attention_heads * cfg.head_dim
         kv_dim = cfg.num_key_value_heads * cfg.head_dim
-        self.q_proj = nn.Linear(cfg.hidden_size, qo_dim, bias=False)
-        self.k_proj = nn.Linear(cfg.hidden_size, kv_dim, bias=False)
-        self.v_proj = nn.Linear(cfg.hidden_size, kv_dim, bias=False)
+
+        self.qkv_proj = nn.Linear(cfg.hidden_size, qo_dim + kv_dim * 2, bias=False)
         self.o_proj = nn.Linear(qo_dim, cfg.hidden_size, bias=False)
         self.q_norm = nn.RMSNorm(cfg.head_dim, eps=cfg.rms_norm_eps)
         self.k_norm = nn.RMSNorm(cfg.head_dim, eps=cfg.rms_norm_eps)
+        self.register_load_state_dict_pre_hook(make_merge_hook(["q", "k", "v"], "qkv"))
 
     def forward(self, x: Tensor, pos_embeds: Tensor, varlen_info: VarlenInfo | None = None) -> Tensor:
-        hidden_shape = (*x.shape[:-1], -1, self.head_dim)
-        q = apply_rope(self.q_norm(self.q_proj(x).view(hidden_shape)), pos_embeds)
-        k = apply_rope(self.k_norm(self.k_proj(x).view(hidden_shape)), pos_embeds)
-        v = self.v_proj(x).view(hidden_shape)
+        qkv = self.qkv_proj(x).view(*x.shape[:-1], -1, self.head_dim)
+        q, k, v = qkv.split((self.num_qo_heads, self.num_kv_heads, self.num_kv_heads), dim=-2)
+        q = apply_rope(self.q_norm(q), pos_embeds)
+        k = apply_rope(self.k_norm(k), pos_embeds)
 
         dropout_p = self.attention_dropout if self.training else 0.0
         out = attention(q, k, v, varlen_info=varlen_info, is_causal=True, dropout_p=dropout_p)
@@ -41,13 +53,14 @@ class Qwen3Attention(nn.Module):
 class Qwen3MLP(nn.Module):
     def __init__(self, cfg: Qwen3Config) -> None:
         super().__init__()
-        self.gate_proj = nn.Linear(cfg.hidden_size, cfg.intermediate_size, bias=False)
-        self.up_proj = nn.Linear(cfg.hidden_size, cfg.intermediate_size, bias=False)
+        self.gate_up_proj = nn.Linear(cfg.hidden_size, cfg.intermediate_size * 2, bias=False)
         self.down_proj = nn.Linear(cfg.intermediate_size, cfg.hidden_size, bias=False)
         self.act_fn = nn.SiLU()
+        self.register_load_state_dict_pre_hook(make_merge_hook(["gate", "up"], "gate_up"))
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+        gate, up = self.gate_up_proj(x).chunk(2, dim=-1)
+        return self.down_proj(self.act_fn(gate) * up)
 
 
 class Qwen3DecoderLayer(nn.Module):
