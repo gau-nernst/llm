138e9601b83c3f95d2f75d07d683d69254ba525c
Thien Tran <gau.nernst@yahoo.com.sg>
2025-10-21 08:25:14 +0800

    update dataset test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,10 @@
 from torch import Tensor
 
 from pretrain import TokenDataset
+from sft import PackedData, PackedDataset
 
 
-def test_shard():
+def test_token_dataset():
     tokenizer_id = "Qwen/Qwen3-0.6B"
     repo_id = "allenai/c4"
     split = "train"
@@ -14,13 +15,38 @@ def test_shard():
     # basic reading
     for _ in range(20):
         data = next(ds)
-    assert isinstance(data, Tensor)
-    assert data.shape == (ds.seqlen + 1,)
+        assert isinstance(data, Tensor)
+        assert data.shape == (ds.seqlen + 1,)
 
     # stateful-ness
-    state_dict = ds.state_dict()
     resumed_ds = TokenDataset(tokenizer_id, repo_id, split, name)
-    resumed_ds.load_state_dict(state_dict)
+    resumed_ds.load_state_dict(ds.state_dict())
 
     for _ in range(5):
         assert (next(ds) == next(resumed_ds)).all()
+
+
+def tets_packed_dataset():
+    tokenizer_id = "Qwen/Qwen3-0.6B"
+    repo_id = "allenai/tulu-3-sft-mixture"
+    split = "train"
+    maxlen = 2048
+
+    ds = PackedDataset(tokenizer_id, repo_id, split, maxlen=maxlen)
+
+    # basic reading
+    for _ in range(20):
+        data = next(ds)
+        assert isinstance(data, PackedData)
+        assert data.inputs.shape[0] <= maxlen
+
+    # stateful-ness
+    resumed_ds = PackedDataset(tokenizer_id, repo_id, split, maxlen=maxlen)
+    resumed_ds.load_state_dict(ds.state_dict())
+
+    for _ in range(5):
+        for this, that in zip(next(ds), next(resumed_ds)):
+            if isinstance(this, Tensor):
+                assert (this == that).all()
+            else:
+                assert this == that
